63354129c6daf6452e975890ff607329bd404a3e
Scott Kaye <skaye@wayfair.com>
2023-10-23 16:23:38 -0400

    Use Dracula theme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,7 +1,8 @@
 function ColorMyPencils(color)
-  color = color or "rose-pine"
-  --  color = color or "tokyonight"
-  vim.cmd.colorscheme(color)
+  -- color = color or "rose-pine"
+  -- color = color or "gruvbox"
+  -- color = color or "dracula"
+ --  vim.cmd.colorscheme(color)
   --  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   --  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
