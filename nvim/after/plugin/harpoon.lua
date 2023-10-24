2cea28328c40dfd5de780415c50271787a29f1f7
ScottyKaye <scottykaye.web@gmail.com>
2023-10-24 10:57:26 -0400

    Moving towards customizing Harpoon and Dracula theme (#10)
    Co-authored-by: Scott Kaye <skaye@wayfair.com>

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,6 +1,12 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
+require("harpoon").setup({
+    menu = {
+      width = 80,
+    }
+})
+
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
@@ -11,3 +17,9 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-S-P>", function() ui.nav_prev() end)
 vim.keymap.set("n", "<C-S-N>", function() ui.nav_next() end)
+vim.api.nvim_create_autocmd({ 'filetype' },
+  { pattern = 'harpoon',
+    callback = function()
+      vim.cmd([[highlight HarpoonBorder guifg=#FFFFA5]])
+      vim.cmd([[highlight HarpoonWindow guifg=#69FF94]])
+    end })
