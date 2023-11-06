959569e38663abdeb5571efbab47b55387eaa591
ScottyKaye <scottykaye.web@gmail.com>
2023-11-06 10:20:30 -0500

    Added emmett (#17)

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -86,7 +86,26 @@ require('mason-lspconfig').setup({
   },
 })
 
+local configs = require('lspconfig/configs')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+require('lspconfig').emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+})
+
+
+-- this always causes incorrect formatting
 -- lsp.format_on_save({
 --   format_opts = {
 --     async = false,
