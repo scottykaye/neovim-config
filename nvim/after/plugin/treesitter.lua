7a45b0f24c83f2a954988cef78b881cca6742b2e
ScottyKaye <scottykaye.web@gmail.com>
2023-12-03 21:27:33 -0500

    Moving everything over (#23)

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "html", "typescript", "markdown", "go", "rust", "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = { "javascript", "html", "typescript", "markdown", "markdown_inline", "go", "rust", "c", "lua",
+    "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
