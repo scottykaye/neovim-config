2b35f56b21fd3687ebab449a3fb221349e19fd9f
Scott Kaye <skaye@wayfair.com>
2024-01-22 15:11:03 -0500

    Making moves my man

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,25 +1,41 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
 
-require("harpoon").setup({
-    menu = {
-      width = 80,
-    }
-})
+local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+harpoon:setup()
 
--- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
--- vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
--- vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
--- vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+-- harpoon:extend({
+--   UI_CREATE = function(cx)
+--     vim.keymap.set("n", "<C-v>", function()
+--       harpoon.ui:select_menu_item({ vsplit = true })
+--     end, { buffer = cx.bufnr })
+--
+--     vim.keymap.set("n", "<C-x>", function()
+--       harpoon.ui:select_menu_item({ split = true })
+--     end, { buffer = cx.bufnr })
+--   end,
+-- })
 
-vim.keymap.set("n", "<C-S-P>", function() ui.nav_prev() end)
-vim.keymap.set("n", "<C-S-N>", function() ui.nav_next() end)
-vim.api.nvim_create_autocmd({ 'filetype' },
-  { pattern = 'harpoon',
-    callback = function()
-      vim.cmd([[highlight HarpoonBorder guifg=#FFFFA5]])
-      vim.cmd([[highlight HarpoonWindow guifg=#69FF94]])
-    end })
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>",
+  function()
+    harpoon.ui:toggle_quick_menu(harpoon:list(),
+      { border = "rounded", title_pos = "center" })
+  end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(0) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+-- vim.api.nvim_create_autocmd({ 'filetype' },
+--  {
+--    pattern = "harpoon",
+--    callback = function()
+--      vim.cmd([[highlight HarpoonBorder guifg=#FFFFA5]])
+--      vim.cmd([[highlight HarpoonWindow guifg=#69FF94]])
+--    end
+--  })
